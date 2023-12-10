770e696ebcf0ddf68f4daa6e68a138c54dff166f
V3NOM1977 <rishabhrawat1977@gmail.com>
2023-12-10 21:12:20 +0530

    Custom Uri Validator, token settings, and our own data to the jwt

--- src/main/java/com/example/OAuth2/configs/SecurityConfig.java
+++ src/main/java/com/example/OAuth2/configs/SecurityConfig.java
@@ -8,6 +8,7 @@ import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,12 +18,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AuthorizationCodeRequestAuthenticationProvider;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -31,7 +37,10 @@ import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.time.Duration;
+import java.util.List;
 import java.util.UUID;
+import java.util.function.Consumer;
 
 @Configuration
 public class SecurityConfig {
@@ -40,11 +49,14 @@ public class SecurityConfig {
     // http://localhost:8080/oauth2/token?client_id=client&redirect_uri=https://springone.io/authorized&grant_type=authorization_code&code=dWlJMGpGlUAPz0sRU1y8suXDyWejo0_B4-WrLP-ks5kSlcdvlGG-u1OxOORvvpm7IMJaC_lMqzTX2Oh6AKHGOb2J4-Hp6PVPvGjLeUQMnWzz6h3Xyy1D0S6czbiTeU8f&code_verifier=qPsH306-ZDDaOE8DFzVn05TkN3ZZoVmI_6x4LsVglQI
 
     /**
-     * This function configures the security filter chain for an OAuth2 authorization server with an
+     * This function configures the security filter chain for an OAuth2
+     * authorization server with an
      * OIDC customizer and a login URL authentication entry point.
      * 
-     * @param http The `http` parameter is an instance of `HttpSecurity`, which is a class provided by
-     * Spring Security. It allows you to configure security for your application's HTTP requests.
+     * @param http The `http` parameter is an instance of `HttpSecurity`, which is a
+     *             class provided by
+     *             Spring Security. It allows you to configure security for your
+     *             application's HTTP requests.
      * @return The method is returning a SecurityFilterChain object.
      */
     @Bean
@@ -53,16 +65,28 @@ public class SecurityConfig {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
 
         http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .authorizationEndpoint(a -> a.authenticationProviders(getAuthorizationEndpointProviders()))
                 .oidc(Customizer.withDefaults());
 
         // The code `http.exceptionHandling(e -> e.authenticationEntryPoint(new
-        // LoginUrlAuthenticationEntryPoint("/login")));` is configuring the authentication entry point
+        // LoginUrlAuthenticationEntryPoint("/login")));` is configuring the
+        // authentication entry point
         // for handling exceptions related to authentication.
         http.exceptionHandling(
                 e -> e.authenticationEntryPoint(
                         new LoginUrlAuthenticationEntryPoint("/login")));
 
         return http.build();
+    }
+
+    private Consumer<List<AuthenticationProvider>> getAuthorizationEndpointProviders() {
+        return providers -> {
+            for (AuthenticationProvider p : providers) {
+                if (p instanceof OAuth2AuthorizationCodeRequestAuthenticationProvider x) {
+                    x.setAuthenticationValidator(new CustomRedirectUriValidator());
+                }
+            }
+        };
     }
 
     @Bean
@@ -93,10 +117,12 @@ public class SecurityConfig {
     }
 
     /**
-     * This function creates and returns a RegisteredClientRepository object with a single
+     * This function creates and returns a RegisteredClientRepository object with a
+     * single
      * RegisteredClient instance.
      * 
-     * @return The method is returning an instance of the `RegisteredClientRepository` interface.
+     * @return The method is returning an instance of the
+     *         `RegisteredClientRepository` interface.
      */
     @Bean
     public RegisteredClientRepository registeredClientRepository() {
@@ -109,16 +135,24 @@ public class SecurityConfig {
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
                 .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .tokenSettings(
+                        TokenSettings.builder()
+                                .accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+                                .accessTokenTimeToLive(Duration.ofSeconds(900))
+                                .build()
+                )
                 .build();
 
         return new InMemoryRegisteredClientRepository(r1);
     }
 
     /**
-     * The function creates and returns an instance of the AuthorizationServerSettings class with
+     * The function creates and returns an instance of the
+     * AuthorizationServerSettings class with
      * default settings.
      * 
-     * @return An instance of the AuthorizationServerSettings class is being returned.
+     * @return An instance of the AuthorizationServerSettings class is being
+     *         returned.
      */
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
@@ -127,7 +161,8 @@ public class SecurityConfig {
     }
 
     /**
-     * The function generates a RSA key pair, creates a JWKSet containing the public and private keys,
+     * The function generates a RSA key pair, creates a JWKSet containing the public
+     * and private keys,
      * and returns it as a JWKSource.
      * 
      * @return The method is returning an instance of `JWKSource<SecurityContext>`.
@@ -148,6 +183,19 @@ public class SecurityConfig {
 
         JWKSet set = new JWKSet(key);
         return new ImmutableJWKSet<>(set);
+    }
+
+    /**
+     * The function adds a custom claim "my_key" with the value "test" to the OAuth2
+     * token.
+     * 
+     * @return The method is returning an instance of
+     *         `OAuth2TokenCustomizer<JwtEncodingContext>`.
+     */
+    public OAuth2TokenCustomizer<JwtEncodingContext> oauth2TokenCustomizer() {
+        return context -> {
+            context.getClaims().claim("my_key", "test");
+        };
     }
 
 }
